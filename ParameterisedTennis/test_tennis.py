1124f33bc15e5c8c88fb7fd7c68531b65ef4a5b6
shivammittal <devoteeofbadrinath@ymail.com>
2025-05-02 18:52:41 +0530

    adding the unit testing with python

--- ParameterisedTennis/test_tennis.py
+++ ParameterisedTennis/test_tennis.py
@@ -6,6 +6,11 @@ from tennis import score_tennis
                          [(0, 0, "Love-All"),
                           (1, 1, "Fifteen-All"),
                           (2, 2, "Thirty-All"),
-                         ])
+                          (2, 1, "Thirty-Fifteen"),
+                          (3, 1, "Forty-Fifteen"),
+                          (4, 1, "Win for Player 1"),
+                          (4, 3, "Advantage Player 1"),
+                          (4, 5, "Advantage Player 2")
+                     ])
 def test_score_tennis(player1_points, player2_points, expected_score):
     assert score_tennis(player1_points, player2_points) == expected_score
--- ParameterisedTennis/test_tennis_unittest.py
+++ ParameterisedTennis/test_tennis_unittest.py
@@ -9,16 +9,10 @@ class TennisTest(unittest.TestCase):
             (0, 0, "Love-All"),
             (1, 1, "Fifteen-All"),
             (2, 2, "Thirty-All"),
+            (2, 1, "Thirty-Fifteen"),
+            (3, 1, "Forty-Fifteen"),
+            (4, 1, "Win for Player 1"),
         ]
         for player1_points, player2_points, expected_score in test_cases:
             with self.subTest(f"{player1_points}, {player2_points} -> {expected_score}"):
                 self.assertEqual(expected_score, score_tennis(player1_points, player2_points))
-
-    def test_0_0_love_all(self):
-        self.assertEqual("Love-All", score_tennis(0,0))
-
-    def test_1_1_fifteen_all(self):
-        self.assertEqual("Fifteen-All", score_tennis(1, 1))
-
-    def test_2_2_thirty_all(self):
-        self.assertEqual("Thirty-All", score_tennis(2, 2))
