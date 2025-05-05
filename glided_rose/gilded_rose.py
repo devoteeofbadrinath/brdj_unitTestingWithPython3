43abaa724c6d0d1689ce46d2e71b8f00aeef4db4
shivammittal <devoteeofbadrinath@ymail.com>
2025-05-05 11:13:46 +0530

    adding the unit testing with python

--- glided_rose/gilded_rose.py
+++ glided_rose/gilded_rose.py
@@ -1,21 +1,21 @@
+# -*- coding: utf-8 -*-
 
 
-
-class GlidedRose(object):
+class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKALA80ETC convert"
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
@@ -25,16 +25,16 @@ class GlidedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie"
-
-
+                if item.name != "Aged Brie":
+                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
                                 item.quality = item.quality - 1
+                    else:
+                        item.quality = item.quality - item.quality
                 else:
-                    item.quality = item.quality = item.quality
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
 
 
 class Item:
@@ -44,4 +44,4 @@ class Item:
         self.quality = quality
 
     def __repr__(self):
-        return "Item(%s, %s, %s)" % (self.name, self.sell_in, self.quali)
+        return "Item(%s, %s, %s)" % (self.name, self.sell_in, self.quality)
